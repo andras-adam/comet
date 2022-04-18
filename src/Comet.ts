e5612dba70f684d57d03ef382c84d21662b0c218
András Ádám <adm.andras@gmail.com>
2022-04-18 17:16:02 +0300

    :hammer: Use node import for http module

--- src/Comet.ts
+++ src/Comet.ts
@@ -1,4 +1,4 @@
-import http from 'http';
+import http from 'node:http';
 import getRawBody from 'raw-body';
 import { match } from 'path-to-regexp';
 import { ICometRequest, IParams } from './CometRequest';
