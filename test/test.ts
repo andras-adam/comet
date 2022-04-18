cb58bc42e30fd8c694fe5ede00f972d54c92e428
András Ádám <adm.andras@gmail.com>
2022-04-18 17:16:43 +0300

    :vertical_traffic_light: Improved tests for pathname matching

--- test/test.ts
+++ test/test.ts
@@ -41,65 +41,57 @@ suite('Health endpoint', () => {
 
 });
 
-suite('Path matching', () => {
+suite('Pathname matching', () => {
 
   suiteSetup(() => {
+    app.get('/', (req: Request, res: Response) => res.ok({ handler: 0 }));
     app.get('/products', (req: Request, res: Response) => res.ok({ handler: 1 }));
     app.get('/products/search', (req: Request, res: Response) => res.ok({ handler: 2 }));
     app.get('/products/:productId', (req: Request, res: Response) => res.ok({ handler: 3 }));
-    app.get('/products/:productId/code', (req: Request, res: Response) => res.ok({ handler: 4 }));
-    app.get('/products/recent', (req: Request, res: Response) => res.ok({ handler: 5 }));
   });
 
   suiteTeardown(() => {
     app.reset();
   });
 
-  test('path with single segment should match correctly', async () => {
-    const res1 = await server.get('/products');
-    expect(res1.status).to.equal(200);
-    expect(res1.body.handler).to.equal(1);
-    const res2 = await server.get('/categories');
-    expect(res2.status).to.equal(404);
+  test('should return status 200 for registered pathname with no segments', async () => {
+    const res = await server.get('/');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.have.property('handler');
+    expect(res.body.handler).to.equal(0);
   });
 
-  test('path with multiple segments should match correctly', async () => {
-    const res1 = await server.get('/products/search');
-    expect(res1.status).to.equal(200);
-    expect(res1.body.handler).to.equal(2);
-    const res2 = await server.get('/categories/search');
-    expect(res2.status).to.equal(404);
+  test('should return status 200 for registered pathname with single segment', async () => {
+    const res = await server.get('/products');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.have.property('handler');
+    expect(res.body.handler).to.equal(1);
   });
 
-  test('path with parameter segment should match correctly', async () => {
-    const res1 = await server.get('/products/123456');
-    expect(res1.status).to.equal(200);
-    expect(res1.body.handler).to.equal(3);
-    const res2 = await server.get('/products/recent');
-    expect(res2.status).to.equal(200);
-    expect(res2.body.handler).to.equal(3);
-    const res3 = await server.get('/products/nosuchpath');
-    expect(res3.status).to.equal(200);
-    expect(res3.body.handler).to.equal(3);
-    const res4 = await server.get('/products/123456/code');
-    expect(res4.status).to.equal(200);
-    expect(res4.body.handler).to.equal(4);
-    const res5 = await server.get('/products/123456/name');
-    expect(res5.status).to.equal(404);
+  test('should return status 200 for registered pathname with multiple segments', async () => {
+    const res = await server.get('/products/search');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.have.property('handler');
+    expect(res.body.handler).to.equal(2);
   });
 
-  test('trailing slash should not affect matching', async () => {
-    const res1 = await server.get('/products/');
-    expect(res1.status).to.equal(200);
-    expect(res1.body.handler).to.equal(1);
-    const res2 = await server.get('/products/search/');
-    expect(res2.status).to.equal(200);
-    expect(res2.body.handler).to.equal(2);
-    const res3 = await server.get('/products/123/');
-    expect(res3.status).to.equal(200);
-    expect(res3.body.handler).to.equal(3);
-    const res4 = await server.get('/categories/');
-    expect(res4.status).to.equal(404);
+  test('should return status 200 for registered pathname with parameter segment', async () => {
+    const res = await server.get('/products/123456');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.have.property('handler');
+    expect(res.body.handler).to.equal(3);
+  });
+
+  test('should return status 200 for registered pathname with trailing slash', async () => {
+    const res = await server.get('/products/');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.have.property('handler');
+    expect(res.body.handler).to.equal(1);
+  });
+
+  test('should return status 404 for unregistered pathname', async () => {
+    const res = await server.get('/categories');
+    expect(res.status).to.equal(404);
   });
 
 });
