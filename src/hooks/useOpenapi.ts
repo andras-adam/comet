e92891f94a993d2db48fb44d690b9aa53908f9c6
DaniFoldi <6977583+DaniFoldi@users.noreply.github.com>
2022-10-17 11:38:06 +0100

    :zap: Add route operations mapping

--- src/hooks/useOpenapi.ts
+++ src/hooks/useOpenapi.ts
@@ -1,9 +1,26 @@
 import { z } from 'zod'
-import { GET } from '../types'
+import { GET, Method } from '../types'
 import { OpenApi, OpenApiOptions } from '../openapi'
-import { registry } from '../routes'
+import { registry, Route, Routes } from '../routes'
 import { useRoute } from './useRoute'
 
+
+function routeToOperation(route?: Route): OpenApi['paths']['/']['get'] | undefined {
+  if (!route) {
+    return
+  }
+
+  const pathParams = route.schemas.params.map().map(el => ({ in: 'path', required: true, ...el }))
+  const queryParams = route.schemas.query.map().map(el => ({ in: 'query', ...el }))
+  const body = route.schemas.body.map()
+  const responses = route.replies ? Object.fromEntries(Object.entries(route.replies).map()) : undefined
+
+  return {
+    parameters: pathParams.concat(queryParams),
+    requestBody: body,
+    responses
+  }
+}
 
 export interface UseOpenApiOptions extends OpenApiOptions {
   pathname?: string
@@ -23,7 +40,20 @@ export function useOpenapi(options: UseOpenApiOptions): void {
     const now = new Date()
     const date = event.query.date ?? `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
     const paths: OpenApi['paths'] = Object.fromEntries(Object.entries(registry).map(([ path, routes ]) => {
-      return [path, routes]
+      const get = routeToOperation(Routes.find(server ?? 'main', path, Method.GET, date))
+      const put = routeToOperation(Routes.find(server ?? 'main', path, Method.PUT, date))
+      const post = routeToOperation(Routes.find(server ?? 'main', path, Method.POST, date))
+      const _delete = routeToOperation(Routes.find(server ?? 'main', path, Method.DELETE, date))
+      const options = routeToOperation(Routes.find(server ?? 'main', path, Method.OPTIONS, date))
+      const head = routeToOperation(Routes.find(server ?? 'main', path, Method.HEAD, date))
+      const patch = routeToOperation(Routes.find(server ?? 'main', path, Method.PATCH, date))
+      const trace = routeToOperation(Routes.find(server ?? 'main', path, Method.TRACE, date))
+
+      const pathItem = {
+        // eslint-disable-next-line sort-keys
+        get, put, post, _delete, options, head, patch, trace
+      }
+      return [ (path.startsWith('/') ? path : `/${path}`) as `/${string}`, pathItem ]
     }))
     const data: OpenApi = { ...options, paths, openapi: '3.1.0' }
     return event.reply.ok(data)
